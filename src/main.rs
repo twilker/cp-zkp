e3d5f5c5eb5f2058153c7169ab9d6425adf1b057
Tobias Wilker <twilker@phoenixcontact.com>
2023-06-29 12:54:11 +0200

    Add event bus test.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,55 @@
-trait DA {
-    fn create(&mut self, id: &str, value: &str) -> ();
+use eventador::{Eventador, SinkExt, StreamExt};
+use tokio::time::{sleep, Duration, Instant};
+use once_cell::sync::Lazy;
+
+static INSTANT: Lazy<Instant> = Lazy::new(|| Instant::now());
+
+pub struct Subscriber {
+    eventbus: Eventador,
+    i: u16
 }
 
-struct Map {
-    pub map: std::collections::HashMap<String, String>
-}
-
-impl Map {
-    fn new() -> Map {
-        Map {
-            map: std::collections::HashMap::new()
-        }
-    }
-}
-
-impl DA for Map {
-    fn create(&mut self, id: &str, value: &str) -> () {
-        self.map.insert(String::from(id), String::from(value));
-    }
-}
-
-struct UseCase<'d, D> 
-where D: DA + Sync + Send
-{
-    data_access: &'d mut D
-}
-
-impl <'d, D> UseCase<'d, D> 
-where D: DA + Sync + Send
-{
-    fn new(data_access: &'d mut D) -> UseCase<'d, D> {
-        UseCase {
-            data_access
+impl Subscriber {
+    pub fn new(i: u16, eventbus: Eventador) -> Self {
+        Self {
+            i, eventbus
         }
     }
 
-    async fn create(&mut self, id: &str, value: &str) -> () {
-        self.data_access.create(id, value);
+    pub async fn start(self) {
+        let mut subscription = self.eventbus.async_subscriber::<Event>();
+        println!("subscribed {}", self.i);
+        let value = subscription.next().await.expect("Something went wrong").value;
+        println!("pre sleep {} - {}ms since start", self.i, INSTANT.elapsed().as_millis());
+        let now = Instant::now();
+        sleep(Duration::from_millis(1000)).await;
+        println!("{}: {:?} - {}ms - {}ms since start", self.i, value, now.elapsed().as_millis(), INSTANT.elapsed().as_millis());
     }
+}
+
+#[derive(Debug)]
+pub struct Event {
+    pub value: u16
 }
 
 #[tokio::main]
-async fn main(){
-    let mut da = Map::new();
-    let mut use_case = UseCase::new(&mut da);
-    use_case.create("1", "2").await;
-    drop(use_case);
-    use_case = UseCase::new(&mut da);
-    use_case.create("3", "4").await;
-    println!("{:?}", da.map);
+async fn main() {
+    let eventbus = Eventador::new(1024).unwrap();
+
+    let mut publisher = eventbus.async_publisher::<Event>(512);
+
+    for i in 1..100 {
+        let subscriber = Subscriber::new(i, eventbus.clone());
+        tokio::spawn(subscriber.start());
+    }
+
+    //wait for events to subscribe
+    sleep(Duration::from_millis(1000)).await;
+
+    println!("sending at {}", INSTANT.elapsed().as_millis());
+    publisher.send(Event { value: 1234 }).await.expect("Something went wrong");
+    println!("send finished");
+
+    sleep(Duration::from_millis(10000)).await;
+    println!("sleep finished");
 }
